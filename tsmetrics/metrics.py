2c88b5267d1269a48c097624cec8ad726a7e6df8
Sier17 <andersonreis.geo@gmail.com>
2019-11-20 17:16:31 -0300

    fix version 0.0.1.5 metrics

--- tsmetrics/metrics.py
+++ tsmetrics/metrics.py
@@ -6,9 +6,9 @@ def get_metrics(series,show=False):
 
 	"""
 	This function return all polar and basic metrics in the following order:
-
-	mean, maximum, minimum, std, sum, amplitude, first slope, area, area 1st quarter,area of 2nd quarter,area of 3th quarter, area of 4th quarter, circle, gyration, polar balance e a angle.
-
+	mean, maximum, minimum, std, sum, amplitude, first slope, area, area 1st quarter,
+	area of 2nd quarter,area of 3th quarter, area of 4th quarter, circle, gyration, 
+	polar balance e a angle.
 	"""
     
     #Remove eventual nans from timeseries
@@ -21,4 +21,4 @@ def get_metrics(series,show=False):
     basicas = basics.ts_basics(ts)
     polares = polar.ts_polar(ts,show)
 
-    return numpy.concatenate((basicas, polares), axis=None)   
+    return numpy.concatenate((basicas, polares), axis=None)
