b9c6b5cd8c0186724e3f4125a2943d1c35c9e9fb
Sier17 <andersonreis.geo@gmail.com>
2020-03-13 13:40:37 -0300

    reset functions import fractals

--- tsmetrics/fractal.py
+++ tsmetrics/fractal.py
@@ -2,7 +2,7 @@ import numpy as np
 from numba import jit
 from math import log, floor
 
-from .utils import *
+from . import utils
 
 all = ['petrosian_fd', 'katz_fd', 'higuchi_fd', 'detrended_fluctuation']
 
