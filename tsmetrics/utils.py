ac489e5b6eaf9d461ad2f9aecca681b994ba954d
Sier17 <andersonreis.geo@gmail.com>
2020-03-13 11:32:25 -0300

    fix log at utils

--- tsmetrics/utils.py
+++ tsmetrics/utils.py
@@ -5,6 +5,67 @@ from shapely import geometry
 from shapely.geometry import MultiPolygon, Polygon, mapping, shape
 from shapely.geometry.polygon import LinearRing
 
+
+@jit('UniTuple(float64, 2)(float64[:], float64[:])', nopython=True)
+def _linear_regression(x, y):
+    """Fast linear regression using Numba.
+    Parameters
+    ----------
+    x, y : ndarray, shape (n_times,)
+        Variables
+    Returns
+    -------
+    slope : float
+        Slope of 1D least-square regression.
+    intercept : float
+        Intercept
+    """
+    n_times = x.size
+    sx2 = 0
+    sx = 0
+    sy = 0
+    sxy = 0
+    for j in range(n_times):
+        sx2 += x[j] ** 2
+        sx += x[j]
+        sxy += x[j] * y[j]
+        sy += y[j]
+    den = n_times * sx2 - (sx ** 2)
+    num = n_times * sxy - sx * sy
+    slope = num / den
+    intercept = np.mean(y) - slope * np.mean(x)
+    return slope, intercept
+
+@jit('i8[:](f8, f8, f8)', nopython=True)
+def _log_n(min_n, max_n, factor):
+    """
+    Creates a list of integer values by successively multiplying a minimum
+    value min_n by a factor > 1 until a maximum value max_n is reached.
+    Used for detrended fluctuation analysis (DFA).
+    Function taken from the nolds python package
+    (https://github.com/CSchoel/nolds) by Christopher Scholzel.
+    Parameters
+    ----------
+    min_n (float):
+        minimum value (must be < max_n)
+    max_n (float):
+        maximum value (must be > min_n)
+    factor (float):
+       factor used to increase min_n (must be > 1)
+    Returns
+    -------
+    list of integers:
+        min_n, min_n * factor, min_n * factor^2, ... min_n * factor^i < max_n
+        without duplicates
+    """
+    max_i = int(floor(log(1.0 * max_n / min_n) / log(factor)))
+    ns = [min_n]
+    for i in range(max_i + 1):
+        n = int(floor(min_n * (factor ** i)))
+        if n > ns[-1]:
+            ns.append(n)
+    return np.array(ns, dtype=np.int64)
+
 def fixseries(time_series):
     
     """
@@ -148,32 +209,4 @@ def check_input(timeseries):
     else:
         raise Exception('Incorrect type: Please use numpy.array as inumpyut.')
 
-@jit('UniTuple(float64, 2)(float64[:], float64[:])', nopython=True)
-def _linear_regression(x, y):
-    """Fast linear regression using Numba.
-    Parameters
-    ----------
-    x, y : ndarray, shape (n_times,)
-        Variables
-    Returns
-    -------
-    slope : float
-        Slope of 1D least-square regression.
-    intercept : float
-        Intercept
-    """
-    n_times = x.size
-    sx2 = 0
-    sx = 0
-    sy = 0
-    sxy = 0
-    for j in range(n_times):
-        sx2 += x[j] ** 2
-        sx += x[j]
-        sxy += x[j] * y[j]
-        sy += y[j]
-    den = n_times * sx2 - (sx ** 2)
-    num = n_times * sxy - sx * sy
-    slope = num / den
-    intercept = np.mean(y) - slope * np.mean(x)
-    return slope, intercept
+
