8682399f55a41d7b4ef7a2ab89f323b89afcbd10
Sier17 <andersonreis.geo@gmail.com>
2020-03-13 14:12:35 -0300

    reset functions import fractals3

--- tsmetrics/utils.py
+++ tsmetrics/utils.py
@@ -26,7 +26,7 @@ def _linear_regression(x, y):
     sxy = 0
     for j in range(n_times):
         sx2 += x[j] ** 2
-        sx += x[j]s
+        sx += x[j]
         sxy += x[j] * y[j]
         sy += y[j]
     den = n_times * sx2 - (sx ** 2)
