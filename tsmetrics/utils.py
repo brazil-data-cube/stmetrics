570980c11189be4fa2bf982c437bf72158a985d4
Sier17 <andersonreis.geo@gmail.com>
2020-03-13 13:22:02 -0300

    chang log_n function - fix numpy2

--- tsmetrics/utils.py
+++ tsmetrics/utils.py
@@ -58,10 +58,10 @@ def _log_n(min_n, max_n, factor):
         min_n, min_n * factor, min_n * factor^2, ... min_n * factor^i < max_n
         without duplicates
     """
-    max_i = int(floor(log(1.0 * max_n / min_n) / log(factor)))
+    max_i = int(numpy.floor(log(1.0 * max_n / min_n) / log(factor)))
     ns = [min_n]
     for i in range(max_i + 1):
-        n = int(floor(min_n * (factor ** i)))
+        n = int(numpy.floor(min_n * (factor ** i)))
         if n > ns[-1]:
             ns.append(n)
     return numpy.array(ns, dtype=numpy.int64)
