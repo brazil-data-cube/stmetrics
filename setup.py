5f9bf3cb73dbf0b486a3263de4dbdc54197e84b2
Sier17 <andersonreis.geo@gmail.com>
2020-03-16 19:48:32 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -21,7 +21,6 @@ setuptools.setup(
     'matplotlib',
     'shapely',
     'descartes',
-    'numba',
     'nolds'
     ],
     classifiers=[
